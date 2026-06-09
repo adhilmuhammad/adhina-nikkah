a4af70f8acfc262d0c44cc8b4c29a3753336ff2c
Adhil Muhammad <adhilmuhammad333@gmail.com>
2026-06-09 23:49:03 +0530

    Update image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,24 +2,52 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://adhina-nikkah-invitation.vercel.app"),
+
   title: "The Nikkah of Adhina & Shaz",
+
   description:
     "Together with their families, you are invited to the Nikkah of Adhina & Shaz. 22 June 2026 • 10:30 AM • Al Saj Amaranta, Trivandrum",
 
   openGraph: {
     title: "The Nikkah of Adhina & Shaz",
+
     description:
       "22 June 2026 • 10:30 AM • Al Saj Amaranta, Trivandrum",
+
     url: "https://adhina-nikkah-invitation.vercel.app",
+
     siteName: "Nikkah Invitation",
+
     images: [
       {
-        url: "/og-image.jpg",
+        url: "https://adhina-nikkah-invitation.vercel.app/og-image.png",
         width: 1200,
         height: 630,
+        alt: "The Nikkah of Adhina & Shaz",
       },
     ],
+
     type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+
+    title: "The Nikkah of Adhina & Shaz",
+
+    description:
+      "22 June 2026 • 10:30 AM • Al Saj Amaranta, Trivandrum",
+
+    images: [
+      "https://adhina-nikkah-invitation.vercel.app/og-image.png",
+    ],
+  },
+
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
   },
 };
 
